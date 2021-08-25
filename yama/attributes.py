921a0fdf99e34680287445115d127d6768864987
robin <robinservoz@gmail.com>
2021-08-24 23:01:06 -0700

    Update

--- yama/attributes.py
+++ yama/attributes.py
@@ -9,26 +9,35 @@ except NameError:
     basestring = str
 
 
+def get_attribute(node, attr):
+    if attr.endswith(']'):
+        split = attr.split('[')
+        index = int(split.pop(-1)[:-1])
+        attr = '['.join(split)
+        return MultiAttribute(node, attr, index)
+    return Attribute(node, attr)
+
+
 class Attribute(object):
     """
     A class for handling a node attribute and sub-attributes.
     """
 
-    def __init__(self, parent, attr=None):
+    def __init__(self, node, attr):
         """
         :param parent (Depend/Attribute): the node or attribute parent to attr.
         :param attr (str):
         """
-        # todo : add handling of Attribute('node.attr') and Attribute('node.attr.attr') and Attribute('node.attr[0]')
-        assert isinstance(parent, (nodes.DependNode, Attribute)) and isinstance(attr, basestring)
-        self._parent = parent
-        self._attr = attr
+        assert isinstance(node, nodes.DependNode)
+        assert isinstance(attr, (basestring, Attribute))
+        self.node = node
+        self.attribute = attr
 
     def __str__(self):
-        return '{}.{}'.format(self.node, self.get_full_attribute())
+        return self.name
 
     def __repr__(self):
-        return "{}('{}')".format(self.__class__.__name__, self.__str__())
+        return "{}({}, '{}')".format(self.__class__.__name__, repr(self.node), self.attribute)
 
     def __getattr__(self, item):
         """
@@ -36,7 +45,7 @@ class Attribute(object):
         :param item (str): the sub-attribute name.
         :return: Attribute object.
         """
-        return Attribute(self, item)
+        return self.attr(item)
 
     def __getitem__(self, item):
         """
@@ -44,101 +53,62 @@ class Attribute(object):
         :param item (int): the attribute index.
         :return: MultiAttribute object.
         """
-        return MultiAttribute(self, item)
+        if isinstance(item, basestring):
+            item = int(item)
+        return MultiAttribute(self.node, self.attribute, item)
+
+    def __add__(self, other):
+        self.name.__add__(other)
+
+    def __radd__(self, other):
+        other.__add__(self.name)
+
+    @property
+    def name(self):
+        return self.node + '.' + self.attribute
+
+    def attr(self, attr):
+        return get_attribute(self.node, self.attribute + '.' + attr)
 
     @property
     def value(self):
-        return mc.getAttr(self.__str__())
+        print('getting', self.name)
+        value = mc.getAttr(self.name)
+        if isinstance(value, list) and len(value) == 1:
+            value = value[0]
+        return value
 
     @value.setter
     def value(self, value):
-        mc.setAttr(self, value)
-
-    @property
-    def node(self):
-        if isinstance(self._parent, Attribute):
-            return self._parent.node
-        else:
-            return self._parent
+        print('setting')
+        print(self, value)
+        mc.setAttr(self.name, value)
 
     def connect_to(self, attr, *args, **kwargs):
-        mc.connectAttr(self, attr, *args, **kwargs)
+        if isinstance(attr, Attribute):
+            attr = attr.name
+        mc.connectAttr(self.name, attr, *args, **kwargs)
 
     def connect_from(self, attr, *args, **kwargs):
-        mc.connectAttr(attr, self, *args, **kwargs)
+        if isinstance(attr, Attribute):
+            attr = attr.name
+        mc.connectAttr(attr, self.name, *args, **kwargs)
 
-    def get_full_attribute(self):
-        """
-        Gets the full attribute from the first attribute after node to self as a string.
-        :return: str
-        """
-        if not isinstance(self._parent, Attribute):
-            return '{}'.format(self._attr)
-        else:
-            return self._parent.get_full_attribute() + '.{}'.format(self._attr)
-
-    @property
-    def longName(self):
-        """
-        The attribute longName without its parent attribute if any.
-        :return: str
-        """
-        return self._attr
-
-    @property
-    def niceName(self):
-        """
-        Gets the attribute niceName.
-        :return: str
-        """
-        return mc.attributeQuery(self._attr, node=self.obj, niceName=True)
-
-    @niceName.setter
-    def niceName(self, value):
-        """
-        Sets the attribute niceName.
-        :param value (str): the new niceName.
-        """
-        mc.addAttr(self, e=True, niceName=value)
-
-    @property
-    def parent(self):
-        """
-        Return the parent node or attribute.
-        :return: Depend or Attribute object.
-        """
-        return self._parent
-
-    def get_parents(self, attrs_only=False):
-        """
-        Gets a list of all the parent attributes and node.
-        Order of the list is [Depend, first Attribute, ..., last Attribute (self.parent)]
-        :param attrs_only (bool): If True returns all the parent attributes without the parent node.
-        :return: list of Depend and or Attributes objects
-        """
-        if not isinstance(self._parent, Attribute):
-            if not attrs_only:
-                return [self._parent]
-            else:
-                return []
-        else:
-            return self._parent.get_parents(attrs_only=attrs_only) + [self._parent]
-    
     @property
     def type(self):
         """
         Returns the type of data currently in the attribute.
         :return: str
         """
-        return mc.getAttr(self, type=True)
+        return mc.getAttr(self.name, type=True)
 
     @property
     def defaultValue(self):
-        return mc.addAttr(self, q=True, defaultValue=True)
-    
+        return mc.addAttr(self.name, q=True, defaultValue=True)
+
     @defaultValue.setter
     def defaultValue(self, value):
-        mc.addAttr(self, e=True, defaultValue=value)
+        mc.addAttr(self.name, e=True, defaultValue=value)
 
     @property
     def hasMinValue(self):
@@ -151,7 +121,7 @@ class Attribute(object):
          which implies the following way of setting it.
         """
         if not bool(value) == bool(self.hasMinValue):
-            mc.addAttr(self, e=True, hasMinValue=value)
+            mc.addAttr(self.name, e=True, hasMinValue=value)
 
     @property
     def minValue(self):
@@ -159,7 +129,7 @@ class Attribute(object):
 
     @minValue.setter
     def minValue(self, value):
-        mc.addAttr(self, e=True, minValue=value)
+        mc.addAttr(self.name, e=True, minValue=value)
 
     @property
     def hasMaxValue(self):
@@ -243,32 +213,22 @@ class MultiAttribute(Attribute):
     """
     A sub-class to Attribute to handle attribute of type multi; for exemple a deformer weights attribute.
     """
-    def __init__(self, parent, index):
+
+    def __init__(self, node, attr, index):
         """
         :param parent (Attribute/MultiAttribute): The parent attribute of this attribute; Needs to be of type Attribute
                                                   or MultiAttribute.
         :param index: The index of this attribute
         """
-        assert isinstance(parent, Attribute) and isinstance(index, int)
-        super(MultiAttribute, self).__init__(parent, '[{}]'.format(index))
-        self._index = index
-
-    def __str__(self):
-        return '{}[{}]'.format(self._parent, self._index)
+        assert isinstance(node, nodes.DependNode) and isinstance(attr, basestring) and isinstance(index, int)
+        super(MultiAttribute, self).__init__(node, attr)
+        self.attribute = attr + '[' + str(index) + ']'
+        self.index = index
+        self.parent_attr = attr
 
     def __repr__(self):
-        return '{}({}, {})'.format(self.__class__.__name__, self._parent, self._index)
+        return '{}({}, {}, {})'.format(self.__class__.__name__, self.node, self.parent_attr, self.index)
 
     @property
-    def index(self):
-        """
-        :return: int
-        """
-        return self._index
-
-    def get_full_attribute(self):
-        """
-        Gets the full attribute from the first attribute after node to self as a string.
-        :return: str
-        """
-        return self._parent.get_full_attribute() + '{}'.format(self._attr)
+    def parent(self):
+        return get_attribute(self.node, self.parent_attr)
