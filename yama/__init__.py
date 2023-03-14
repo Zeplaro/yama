5e0b6bdc586abdbf187889710fc9db379f09933b
robin <robinservoz@gmail.com>
2023-03-13 19:24:04 -0700

    Added import duplicate

--- yama/__init__.py
+++ yama/__init__.py
@@ -5,6 +5,6 @@ __email__ = "contact@robinlavigne.com"
 __credits__ = {'emotionalSupport': "Emilie Jolin", 'spellchecking': "Pranil Naicker"}
 
 
-from .nodes import gmo, yam, yams, createNode, ls, selected, select, YamList, spaceLocator
+from .nodes import gmo, yam, yams, createNode, ls, selected, select, YamList, spaceLocator, duplicate
 
 yum = nodes.Yum()
