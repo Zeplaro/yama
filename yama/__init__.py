89e526b42e3f6385a68102b104511989c694ee50
robin <robinservoz@gmail.com>
2025-10-11 17:33:27 -0700

    Integrate Maya commands and decorators into the initialization module

--- yama/__init__.py
+++ yama/__init__.py
@@ -4,7 +4,9 @@ __author__ = "Robin Lavigne"
 __email__ = "contact@robinlavigne.com"
 __credits__ = {"emotionalSupport": "Emilie Jolin"}
 
-
+from maya import cmds
 from .nodes import *
+from . import decorators
 
 yum = nodes.Yum()
+ymmds = decorators.Yammds(cmds)
